b1ee96bd431469042aa3be76a01d5ea878d28fd8
shivashankaregowda <gowda.svarks@gmail.com>
2019-07-30 11:25:06 +0530

    Customer dashboard reset password added

--- src/app/customer/shared/components/header/header.component.ts
+++ src/app/customer/shared/components/header/header.component.ts
@@ -1,21 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import {Routes, Router, ActivatedRoute} from '@angular/router';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [NgbModal, NgbModalConfig],
 })
 export class HeaderComponent implements OnInit {
+  resetForm: FormGroup;
+  submitted = false;
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute,
+              private config: NgbModalConfig, private modalService: NgbModal,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
 
-  signout()
-  {
-    this.router.navigate(['login'])
+  signout() {
+    this.router.navigate(['login']);
   }
+  resetPwdOpen(resetPWD) {
+      this.modalService.open(resetPWD);
+      this.resetForm = this.formBuilder.group ({
+        oldPwd: ['', [Validators.required]],
+        newPwd: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPwd: ['', [Validators.required, Validators.minLength(6)]],
+      },
+      {
+        validator: this.passwordMatchValidator
+      });
+  }
+passwordMatchValidator(frm: FormGroup) {
+    return frm.controls['newPwd'].value === frm.controls['confirmPwd'].value ? null : {'mismatch': true};
+  }
+  resetPassword() {
+    this.submitted = true;
+    if (this.resetForm.invalid) {
+      return;
+    }
 
+    console.log(this.resetForm.value);
+  }
 }
+
--- src/app/customer/shared/shared.module.ts
+++ src/app/customer/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { NgbModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MainpageComponent} from './pages/mainpage/mainpage.component'
+import {MainpageComponent} from './pages/mainpage/mainpage.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -11,10 +13,16 @@ import { RouterModule, Routes } from '@angular/router';
   declarations: [MainpageComponent, FooterComponent, HeaderComponent, SidebarComponent, MainpageComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgbModule,
+    NgbDropdownModule,
+    ReactiveFormsModule
   ],
-  exports: [MainpageComponent]
- 
-  
+  exports: [
+    MainpageComponent,
+    NgbModule,
+    NgbDropdownModule
+  ]
+
 })
 export class SharedModule { }
