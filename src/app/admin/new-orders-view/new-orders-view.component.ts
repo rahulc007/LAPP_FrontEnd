d9d978163122dccd0618497e8c8b0186d9befeb4
mohithkumarsvarks <mohith.svarks@gmail.com>
2019-08-18 17:36:22 +0530

    New orders Api integrated for Admin & orders view also updated

--- src/app/admin/new-orders-view/new-orders-view.component.ts
+++ src/app/admin/new-orders-view/new-orders-view.component.ts
@@ -3,6 +3,8 @@ import {ConfigurationService} from '../../common/ngx-easy-table/config-service';
 import {UserService} from '../../core/services/user.service';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Route, Router} from '@angular/router';
+import { userTypes } from '../../common/constants/constants';
+import { LappRestService } from '../../core/rest-service/LappRestService';
 
 @Component({
   selector: 'app-new-orders-view',
@@ -18,26 +20,39 @@ export class NewOrdersViewComponent implements OnInit {
   pageOfItems = [];
   baseUrl:any;
   public data :any[]=[];
- 
-  constructor(private UserService:UserService,private http: HttpClient, private route: ActivatedRoute,private router:Router) {this.configuration = ConfigurationService.config; 
+  params: any;
+  constructor(private UserService:UserService,private http: HttpClient, private route: ActivatedRoute,
+    private router:Router, private objService: LappRestService) {
     this.configuration = ConfigurationService.config;
   }
 
   ngOnInit() {
-    this.route.queryParams.subscribe(x => this.loadPage(x.page || 1))
+    this.route.queryParams.subscribe(x => this.loadPage(x.page || 1));
+    this.getUploadedOrderDetails();
   }
-
+ getUploadedOrderDetails() {
+   let i= localStorage.getItem('index');
+   console.log('index',i)
+    let objUserDetails = JSON.parse(localStorage.getItem('currentUser'));
+    const emailId = localStorage.getItem('username');
+    if (objUserDetails.userType === userTypes.superAdmin || objUserDetails.userType === userTypes.admin) {
+      this.objService.Get('getOrderDetailsByAdmin?emailId=' + emailId, this.params).subscribe(response => {
+          console.log("new order view response", response)
+          this.data= response.orderInfoList[i].orderLineItem;
+      })
+    }
+  }
   ngAfterViewInit() {
     this.columns = [
-      { key: 'id', title: 'Line Item No' },
-      { key: 'name', title: 'Article No' },
-      { key: 'name', title: 'Art Design' },
-      { key: 'name', title: 'Length' },
-      { key: 'name', title: 'Qunatity' },
-      { key: 'name', title: 'Marking Text Left' },
-      { key: 'name', title: 'Marking Text Right' },
-
-      
+      { key: 'customerNo', title: 'Customer Number' },
+      { key: 'customerPartNo', title: 'Customer Part Number' },
+      { key: 'articleNo', title: 'Article Number' },
+      { key: 'length', title: 'Length' },
+      { key: 'lineItemId', title: 'Line Item ID' },
+      { key: 'lineItemno', title: 'Line Item Number' },
+      { key: 'prodOrderno', title: 'Product Order Number' },
+      { key: 'quantity' , title:'Quantity'},
+      { key: 'updatedBy' , title:'Updated By'}
     ]
 
   }
@@ -50,6 +65,8 @@ export class NewOrdersViewComponent implements OnInit {
         this.data = this.pageOfItems
     });
   }
-
+  goPrevious() {
+    this.router.navigate(['admin/neworders']);
+  }
 
 }
--- src/app/admin/new-orders/new-orders.component.ts
+++ src/app/admin/new-orders/new-orders.component.ts
@@ -1,57 +1,72 @@
-import { Component, OnInit,ViewChild, TemplateRef, AfterViewInit } from '@angular/core';
-import {ConfigurationService} from '../../common/ngx-easy-table/config-service';
-import {UserService} from '../../core/services/user.service';
+import { Component, OnInit, ViewChild, TemplateRef, AfterViewInit } from '@angular/core';
+import { ConfigurationService } from '../../common/ngx-easy-table/config-service';
+import { UserService } from '../../core/services/user.service';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Route, Router} from '@angular/router';
-
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { userTypes } from '../../common/constants/constants';
+import { LappRestService } from '../../core/rest-service/LappRestService';
 @Component({
   selector: 'app-new-orders',
   templateUrl: './new-orders.component.html',
   styleUrls: ['./new-orders.component.css']
 })
-export class NewOrdersComponent implements OnInit , AfterViewInit{
-  @ViewChild('ver',{static: false}) Ver: TemplateRef<any>;
+export class NewOrdersComponent implements OnInit, AfterViewInit {
+  @ViewChild('ver', { static: false }) Ver: TemplateRef<any>;
   configuration: any;
   public columns: any[] = [];
   pager = {};
   pageOfItems = [];
-  baseUrl:any;
-  public data :any[]=[];
- 
-  constructor(private UserService:UserService,private http: HttpClient, private route: ActivatedRoute,private router:Router) {
-    this.configuration = ConfigurationService.config; 
+  baseUrl: any;
+  public data: any[] = [];
+  params: any;
+  constructor(private UserService: UserService, private http: HttpClient, private route: ActivatedRoute,
+    private router: Router, private objService: LappRestService) {
+    this.configuration = ConfigurationService.config;
   }
 
   ngOnInit() {
-   
-    this.route.queryParams.subscribe(x => this.loadPage(x.page || 1))
-
+    this.route.queryParams.subscribe(x => this.loadPage(x.page || 1));
+    this.getUploadedOrderDetails();
   }
-  
+  getUploadedOrderDetails() {
+    let objUserDetails = JSON.parse(localStorage.getItem('currentUser'));
+    const emailId = localStorage.getItem('username');
+    if (objUserDetails.userType === userTypes.superAdmin || objUserDetails.userType === userTypes.admin) {
+      this.objService.Get('getOrderDetailsByAdmin?emailId=' + emailId, this.params).subscribe(response => {
+          console.log("new order response", response)
+          this.data= response.orderInfoList;
+      })
+    }
+  }
   ngAfterViewInit() {
     this.columns = [
-      { key: 'id', title: 'User ID' },
-      { key: 'name', title: 'Order No' },
-      { key: 'name', title: 'Ordered Date' },
-      { key: 'name', title: 'Updated Date' },
-      {key: 'Actions', title: 'View', searchEnabled: false, cellTemplate: this.Ver}
+      { key: 'userEmailId', title: 'User Email ID' },
+      { key: 'oid', title: 'Order ID' },
+      { key: 'orderDate', title: 'Order Date' },
+      { key:'productionOrderStatus', title:'Production Ordrer Status'},
+      { key:'productionOrderno', title:'Production Order Number'},
+      { key:'countryCode', title:'Country Code'},
+      { key: 'createdDate', title: 'Created Date' },
+      {key:'modifiedDate', title:'Modified Date'},
+      {key:'createdBy' ,title:'Created By'},
+      { key: 'Actions', title: 'View', searchEnabled: false, cellTemplate: this.Ver }
     ]
 
   }
 
   private loadPage(page) {
     // get page of items from api
-    this.http.get<any>(`http://localhost:4000/items?page=${page }`).subscribe(x => {
-        this.pager = x.pager;
-        this.pageOfItems = x.pageOfItems;
-        this.data = this.pageOfItems
+    this.http.get<any>(`http://localhost:4000/items?page=${page}`).subscribe(x => {
+      this.pager = x.pager;
+      this.pageOfItems = x.pageOfItems;
+      this.data = this.pageOfItems
     });
   }
 
-  ordersview(row)
-  {
-    console.log("row===>",row);
-    this.router.navigate(["admin/neworders/newordersview", row.id])
+  ordersview(row, rowIndex) {
+    console.log(rowIndex);
+    localStorage.setItem('index', rowIndex);
+    this.router.navigate(['admin/newordersview', row.oid])
   }
 
 }
