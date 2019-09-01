b45bd2bfbec7230d3aadc74d5256fe9a312af424
Preetam <preetam.sortur@gmail.com>
2019-09-01 13:02:55 +0530

    sidebar navigation responsiveness and auto hide functionality added

--- src/app/common/nav-toggle-service/navtoggle.service.ts
+++ src/app/common/nav-toggle-service/navtoggle.service.ts
@@ -9,19 +9,8 @@ import { $ } from 'protractor';
 export class NavToggleService {
 
     showSideBar:boolean = true;
-    screenWidth;
-    resolutionChanged:boolean = false;
     
     toggleSideBar(){
-      // if(!this.resolutionChanged && window.innerWidth<768){
-      //   console.log("sidebar--",this.showSideBar);
-      //   this.showSideBar = true;
-      //   this.resolutionChanged = true;
-      // } else {
         this.showSideBar = !this.showSideBar;
-      // }
-
-
-
     }
 }
